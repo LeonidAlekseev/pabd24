da609a5d86561e50b6c51e8b626b4563512ed205
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2024-06-25 03:43:25 +0300

    Deprecate print

--- grads/service_test.py
+++ grads/service_test.py
@@ -47,7 +47,7 @@ def test_100(endpoint, name):
     for row in tqdm.tqdm(records):
         t, price = do_request(row, endpoint)
         delays.append(t)
-        print(f'Price: {price}, delay: {t}')
+        # print(f'Price: {price}, delay: {t}')
         pred_prices.append(price)
     avg_delay = sum(delays) / len(delays)
     error = np.array(pred_prices) - prices.to_numpy()
